163b4cad72dcc5af4ea33c335f8ff35b4de3e42c
alphaseinor <alphaseinor@gmail.com>
2019-10-30 21:01:10 -0500

    added state and method for handling completed task

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+
+    this.state = {
+      task: data
+    };
+  }
+
+  completedTask(taskId) {
+    console.log(`completedTask: ${taskId} `);
+  }
+
   render() {
     return (
       <div>
